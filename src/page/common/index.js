b9802495ceb654bc4f68404b14a4517360a6d557
sunyujie <a247304496@126.com>
2018-03-19 23:21:43 +0800

    initial

---
+++ src/page/common/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Sun Yu Jie
+* @Date:   2018-03-19 21:16:30
+* @Last Modified by:   Sun Yu Jie
+* @Last Modified time: 2018-03-19 21:16:51
+*/
+
+console.log('i am global');
---
+++ src/page/module.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Sun Yu Jie
+* @Date:   2018-03-19 21:14:42
+* @Last Modified by:   Sun Yu Jie
+* @Last Modified time: 2018-03-19 21:14:57
+*/
+
+module.exports = {
+  test:1234
+}
